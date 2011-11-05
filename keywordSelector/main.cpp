35620da65b5406769b54f2220623626a20e20596
Michał Janiszewski <janisozaur@gmail.com>
2011-11-05 20:36:54 +0100

    use command line argument as the file to extract keywords from

--- keywordSelector/main.cpp
+++ keywordSelector/main.cpp
@@ -8,8 +8,11 @@
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    if (a.arguments().size() < 2) {
+        qFatal("usage: %s [input file]\n", argv[0]);
+    }
     
-    const QList<QPair<QString, QString> > articles = ArticleLoader::loadFromFile("test.dat");
+    const QList<QPair<QString, QString> > articles = ArticleLoader::loadFromFile(a.arguments().at(1));
     KeywordExtractorInterface *kwi = new DiscriminatingExtractor();
     const QList<QPair<QString, double> > kw = kwi->extractKeywords(articles);
     delete kwi;
